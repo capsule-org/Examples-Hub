c507c36c25808398b790bf2cf957e987fadd269c
Jordy McNab <jordy@usecapsule.com>
2024-10-08 02:03:40 -0500

    chore: update next dependencies

--- web/with-nextjs/next.config.mjs
+++ web/with-nextjs/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: [
+    "@usecapsule/rainbowkit",
+    "@usecapsule/rainbowkit-wallet",
+    "@usecapsule/core-components",
+    "@usecapsule/react-components",
+    "@usecapsule/react-sdk",
+    "@usecapsule/core-sdk",
+    "@usecapsule/web-sdk",
+    "@usecapsule/wagmi-v2-integration",
+    "@usecapsule/viem-v2-integration",
+    "@usecapsule/shared-examples",
+  ],
+};
 
 export default nextConfig;
