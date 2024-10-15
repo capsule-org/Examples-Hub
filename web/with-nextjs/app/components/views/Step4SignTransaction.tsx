2e29c379aea6af38e0012a8d6bce9a2a72fcac1c
Jordy McNab <jordy@usecapsule.com>
2024-10-15 08:16:17 -0500

    chore: file rename and add signing switch to step4

--- web/with-nextjs/app/components/views/Step4SignTransaction.tsx
+++ web/with-nextjs/app/components/views/Step4SignTransaction.tsx
@@ -1,5 +1,8 @@
 import React, { PropsWithChildren } from "react";
 import StepLayout from "../layouts/stepLayout";
+import { useAtom } from "jotai";
+import { selectedSignerAtom } from ".state";
+import SignWithEthers from ".signing/SignWithEthers";
 
 type Step4SignTransactionProps = {};
 
@@ -8,10 +11,32 @@ const SUBTITLE =
   "Sign a transaction or UserOperation with the selected library. Reference the code snippets on the right to see how to sign a transaction.";
 
 const Step4SignTransaction: React.FC<PropsWithChildren<Step4SignTransactionProps>> = () => {
+  const [selectedSigner] = useAtom(selectedSignerAtom);
+
+  const renderSignComponent = () => {
+    switch (selectedSigner) {
+      case "capsule-client":
+        return <div>Sign with CosmosWasm</div>;
+      case "ethers":
+        return <SignWithEthers />;
+      case "viem":
+        return <div>Sign with EthereumWallet</div>;
+      case "solana-web3js":
+        return <div>Sign with SolanaWallet</div>;
+      case "cosmjs":
+        return <div>Sign with TezosWallet</div>;
+      case "alchemy-aa":
+        return <div>Sign with AlchemyWallet</div>;
+      default:
+        return <div>Please select a signing method</div>;
+    }
+  };
   return (
     <StepLayout
       title={TITLE}
-      subtitle={SUBTITLE}></StepLayout>
+      subtitle={SUBTITLE}>
+      {renderSignComponent()}
+    </StepLayout>
   );
 };
 
