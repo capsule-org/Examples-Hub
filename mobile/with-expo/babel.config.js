64cfcb2b1083af1b8f49f4c073cb51a5659bccf4
Jordy McNab <jordy@usecapsule.com>
2024-10-23 13:32:24 -0500

    feat: update with-expo to match rn

--- mobile/with-expo/babel.config.js
+++ mobile/with-expo/babel.config.js
@@ -1,17 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      ["module:react-native-dotenv"],
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@rn-integration-examples/shared-ui": "../shared-ui/src",
-          },
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
