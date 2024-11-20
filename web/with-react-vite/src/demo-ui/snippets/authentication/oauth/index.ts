6ff2b50a0f053a6e85e283dbab4cf2a70acaf78d
Jordy McNab <jordy@usecapsule.com>
2024-11-19 21:11:47 -0600

    chore: remove files

--- web/with-react-vite/src/demo-ui/snippets/authentication/oauth/index.ts
+++
@@ -1,8 +0,0 @@
-// index.ts
-import { CodeStepItem } from "../../../types";
-import { setupSteps } from "./setup";
-import { authSteps } from "./auth";
-
-const steps: CodeStepItem[][] = [setupSteps, authSteps];
-
-export default steps;
