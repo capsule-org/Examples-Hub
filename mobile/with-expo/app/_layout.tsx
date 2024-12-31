4755c1a06542a57da264d6e823479a456b3ce671
Jordy McNab <jordy@usecapsule.com>
2024-12-31 12:35:03 -0600

    chore: minimum dependencies

--- mobile/with-expo/app/_layout.tsx
+++ mobile/with-expo/app/_layout.tsx
@@ -1,7 +1,13 @@
+import "@usecapsule/react-native-wallet/shim";
 import { ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { DefaultTheme } from "@react-navigation/native";
+
+console.log(global.crypto.getRandomValues(new Uint8Array(8)));
+console.log(new global.TextEncoder().encode("Hello!"));
+console.log(global.atob("SGVsbG8gV29ybGQ="));
+console.log(new Blob(["Testing"]).stream);
 
 export default function RootLayout() {
   return (
