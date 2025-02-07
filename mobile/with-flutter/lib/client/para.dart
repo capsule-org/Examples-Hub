5a108e32b06dedde11850b247bafe68f5abcf514
Brian Corbin <brian.william.corbin@gmail.com>
2025-02-07 09:27:09 -0800

    updating env to para

--- mobile/with-flutter/lib/client/para.dart
+++ mobile/with-flutter/lib/client/para.dart
@@ -3,9 +3,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:para_metamask_connector/para_metamask_connector.dart';
 import 'package:para_phantom_connector/para_phantom_connector.dart';
 
-final environment = dotenv.env['CAPSULE_ENV'] ?? 'beta';
-final apiKey = dotenv.env['CAPSULE_API_KEY'] ??
-    (throw Exception('CAPSULE_API_KEY not found in .env file'));
+final environment = dotenv.env['PARA_ENV'] ?? 'beta';
+final apiKey = dotenv.env['PARA_API_KEY'] ??
+    (throw Exception('PARA_API_KEY not found in .env file'));
 
 final paraClient = Para(
   environment:
